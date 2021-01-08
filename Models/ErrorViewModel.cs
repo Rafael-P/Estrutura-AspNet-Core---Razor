31097a5bec05c7678dd2c45da439c2cca3bcfc9f
Rafael-P <r4f4el_porto@hotmail.com>
2021-01-08 10:11:19 -0300

    Estrutura AspNet Core - Razor / adicionada

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Estrutura_AspNet_Core___Razor.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
